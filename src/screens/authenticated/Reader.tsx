0354e4ea64b2bb558d5778df6d9b83cd36e348c7
Quentin Roux <quentin@monpetitgazon.com>
2022-12-31 18:36:52 +0100

    [EXPO] expo Speech module

--- src/screens/authenticated/Reader.tsx
+++ src/screens/authenticated/Reader.tsx
@@ -4,6 +4,7 @@ import {launchImageLibrary} from 'react-native-image-picker';
 
 import {RootNavigatorStackParamList} from '@navigators/RootNavigator';
 import {StackScreenProps} from '@react-navigation/stack';
+import * as Speech from 'expo-speech';
 
 import {recognizeImage} from '../../lib/MLKit';
 
@@ -34,11 +35,24 @@ const Reader: FC<ReaderScreenProps> = () => {
     }
   };
 
+  // const onReadPress = () => {
+  //   if (text) {
+  //     Speech();
+  //     Speech.speak('lorem ipsum doloris');
+  //   }
+  // };
+
+  const speak = () => {
+    const thingToSay = '1 2 3 4 5';
+    Speech.speak(thingToSay);
+  };
+
   return (
     <View>
       <Text>Reader</Text>
       <Button title="open lib" onPress={onSelectImagePress} />
       <Button title="extract text" onPress={processImage} />
+      <Button title="read" onPress={speak} />
       <Text>{text}</Text>
     </View>
   );
