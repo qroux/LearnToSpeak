c23bf5a50022aa1f31acc4182772b993ba64f590
Quentin Roux <quentin@monpetitgazon.com>
2022-12-31 17:47:29 +0100

    [FEAT] MLKit textRecognition IOS => working with rosetta (react-native-vision-camera)

---
+++ ios/MLKit/RCTTextRecognitionModule.h
@@ -0,0 +1,16 @@
+//
+//  RCTTextRecognititionModule.h
+//  LearnToSpeak
+//
+//  Created by Quentin Roux on 31/12/2022.
+//
+
+#ifndef RCTTextRecognitionModule_h
+#define RCTTextRecognitionModule_h
+
+#import <React/RCTBridgeModule.h>
+@interface RCTTextRecognitionModule : NSObject <RCTBridgeModule>
+@end
+
+
+#endif /* RCTTextRecognititionModule_h */
--- src/screens/authenticated/Reader.tsx
+++ src/screens/authenticated/Reader.tsx
@@ -1,8 +1,11 @@
-import React, {FC} from 'react';
-import {Text, View} from 'react-native';
+import React, {FC, useState} from 'react';
+import {Button, Text, View} from 'react-native';
+import {launchImageLibrary} from 'react-native-image-picker';
 
 import {RootNavigatorStackParamList} from '@navigators/RootNavigator';
 import {StackScreenProps} from '@react-navigation/stack';
+
+import {recognizeImage} from '../../lib/MLKit';
 
 type ReaderScreenProps = StackScreenProps<
   RootNavigatorStackParamList,
@@ -10,9 +13,33 @@ type ReaderScreenProps = StackScreenProps<
 >;
 
 const Reader: FC<ReaderScreenProps> = () => {
+  const [selectedUri, setSelectedUri] = useState<string | undefined>();
+  const [text, setText] = useState<string | undefined>();
+
+  const onSelectImagePress = () => {
+    launchImageLibrary({selectionLimit: 1, mediaType: 'photo'}, res => {
+      const uri = res?.assets?.[0].uri;
+
+      if (uri) {
+        setSelectedUri(uri);
+      }
+    });
+  };
+
+  const processImage = () => {
+    if (selectedUri) {
+      recognizeImage(selectedUri)
+        .then(response => setText(response?.blocks?.[0].text))
+        .catch(err => console.log({err}));
+    }
+  };
+
   return (
     <View>
       <Text>Reader</Text>
+      <Button title="open lib" onPress={onSelectImagePress} />
+      <Button title="extract text" onPress={processImage} />
+      <Text>{text}</Text>
     </View>
   );
 };
